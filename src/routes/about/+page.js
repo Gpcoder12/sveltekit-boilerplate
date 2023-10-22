627b7532178407f95bd93412400613f89ca49269
Gopolang <mogashoag@yahoo.com>
2023-10-22 08:12:19 +0200

    Update About.js

--- src/routes/about/+page.js
+++ src/routes/about/+page.js
@@ -1,6 +1,6 @@
 import { dev } from '$app/environment';
 
-// we don't need any JS on this page, though we'll load
+our company has founded in 2017 by Gopolang this page, though we'll load
 // it in dev so that we get hot module replacement
 export const csr = dev;
 
